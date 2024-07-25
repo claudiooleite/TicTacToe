18a5a641a718fd0f76a0f69972a1299450c179d4
claudiooleite <claudio.leite@hotmail.com>
2024-07-25 10:43:43 +0200

    game finished

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
 //  9 options
 //
 // 2 players
-const players = ["X", "O"];
-// current player
-let currentPlayer = players[0];
+
+// function that has the board
+// function that updates the board
+// function that when player moves call the function that updates the board
+// function that checks the board
+// if the board has certain moves determins the winner
+// clear the board
+//
 
 const winning_combinations = [
   [0, 1, 2],
@@ -18,16 +23,6 @@ const winning_combinations = [
 // board
 const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-function Game(player, position) {
-  //   const newBoard = [
-  //     ...board.slice(0, position),
-  //     player,
-  //     ...board.slice(position + 1),
-  //   ];
-  board.splice(position, 1, player);
-  checkWinning(board, player);
-  return board;
-}
 function checkWinning(board, player) {
   for (const combination of winning_combinations) {
     const [a, b, c] = combination;
@@ -37,15 +32,37 @@ function checkWinning(board, player) {
   }
   return false;
 }
+// factory function
+// Function to create a player
+function createPlayer(symbol) {
+  return {
+    symbol,
+    plays(position) {
+      // Validate the move
+      if (typeof board[position] === "number") {
+        // Update the board with the player's symbol
+        board[position] = this.symbol;
 
-const play = Game(players[0], board[2]);
-const play1 = Game(players[1], board[1]);
-const play2 = Game(players[0], board[0]);
-const play3 = Game(players[1], board[3]);
-const play4 = Game(players[0], board[4]);
-const play5 = Game(players[1], board[5]);
-const play6 = Game(players[0], board[8]);
-const play7 = Game(players[1], board[7]);
-const play8 = Game(players[0], board[6]);
+        // Check if this move wins the game
+        if (checkWinning(board, this.symbol)) {
+          console.log(`Player ${this.symbol} wins!`);
+          console.log("Final Board:", board);
+        } else {
+          console.log(`Player ${this.symbol} played. Board:`, board);
+        }
+      } else {
+        console.log("Invalid move! Position already taken.");
+      }
+    },
+  };
+}
+// Create players
+const playerX = createPlayer("X");
+const playerO = createPlayer("O");
 
-console.log(play5);
+// Example moves
+playerX.plays(2); // Player X moves
+playerO.plays(4); // Player O moves
+playerX.plays(6); // Player X moves
+playerX.plays(7); // Player X moves
+playerX.plays(8); // Player X moves
